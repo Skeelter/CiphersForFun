357bdd586d9d61b8b0efb2113c1993cc56be571c
Skeelter <lesociopathedetresbasniveau@gmail.com>
2021-09-25 13:11:50 +0200

    Atbash added

---
+++ atbash.py
@@ -0,0 +1,47 @@
+import string
+
+#Définition des variables nécessaires
+alphabet = string.ascii_lowercase
+reversedAlphabet = string.ascii_lowercase [::-1]
+upperAlphabet = string.ascii_uppercase
+reversedUpperAlphabet = string.ascii_uppercase [::-1]
+specialChar = "@_!#$%^&*()?/\|}{~:]’"
+
+#Fonction d'encryption
+def EncryptAtbash():
+    #Données nécessaires
+    message = input("Your message : ")
+    message = list(message)
+    encrypted = ""
+    
+    #Encryption
+    for letter in message:
+        if letter in alphabet:
+            encrypted += reversedAlphabet[alphabet.index(letter)]
+        elif letter in upperAlphabet:
+            encrypted += reversedUpperAlphabet[upperAlphabet.index(letter)]
+        else:
+            encrypted += letter
+
+    #Output
+    print("Encrypted message :", encrypted, "\n")
+
+
+#Fonction de decryptage
+def DecryptAtbash():
+    #Données nécessaires
+    message = input("Initial message : ")
+    message = list(message)
+    decrypted = ""
+    
+    #Encryption
+    for letter in message:
+        if letter in alphabet:
+            decrypted += reversedAlphabet[alphabet.index(letter)]
+        elif letter in upperAlphabet:
+            decrypted += reversedUpperAlphabet[upperAlphabet.index(letter)]
+        else:
+            decrypted += letter
+
+    #Output
+    print("Decrypted message :", decrypted, "\n")
